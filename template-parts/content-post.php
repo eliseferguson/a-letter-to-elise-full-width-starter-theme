eabb2066487e65759b1bee1b239c773909bfa1df
eliseferguson <egaetz@gmail.com>
2017-10-19 09:48:14 -0500

    Basic styles for single blog post

--- template-parts/content-post.php
+++ template-parts/content-post.php
@@ -8,12 +8,13 @@
  */
 
 ?>
-<!-- egf content-single.php -->
+<!-- egf content-post.php-->
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 
 	<div class="entry-content">
 		<?php
+			echo "<h2 class='post-title'>" . get_the_title() . "</h2>";
 			the_content();
 
 			wp_link_pages( array(
